9cd41fe5b9b4215893da0f92feee390429eab58c
karolip.matias <karolinyp.matias@gmail.com>
2022-06-27 20:34:35 -0300

    exercícios sobre declaração de variáveis

---
+++ variaveis/script.js
@@ -0,0 +1,21 @@
+// Declarar uma variável com o seu nome
+var nome = 'Karoliny';
+
+// Declarar uma variável com a sua idade
+var idade = 30;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita; 
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Pizza';
+
+// Declarar 5 variáveis diferentes sem valores
+var cor = 'vermelho', 
+    cidade = 'Brasília',
+    $mae = 'Maria'
+    $pai = 'Salvador'
+    _irma = 'Rebeca'
+
+console.log(nome,idade,comidaFavorita,cor,cidade,$mae,$pai,_irma)
