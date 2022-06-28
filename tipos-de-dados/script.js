3c039fe55c0b5020940f5478a5cb9271e0813dd4
karolip.matias <karolinyp.matias@gmail.com>
2022-06-27 21:09:30 -0300

    atividade sobre string e template string

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,23 @@
+// Declare uma variável contendo uma string
+var corFavorita = 'vermelho';
+
+// Declare uma variável contendo um número dentro de uma string
+var numero = '10';
+
+// Declare uma variável com a sua idade
+var idade = 30;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = 'Karoliny',
+    sobrenome = 'Pires';
+    nomeSobrenome =  `Meu nome é: ${nome} ${sobrenome}`;
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = 'It\'s time';
+
+// Verifique o tipo da variável que contém o seu nome
+
+console.log(corFavorita, numero,nomeSobrenome, frase);
+
+console.log(typeof nomeSobrenome)
