67ade9982d7f7219884aa48722a3b446c129e791
karolip.matias <karolinyp.matias@gmail.com>
2022-07-04 12:49:24 -0300

    atividades sobre objeto

---
+++ objetos/script.js
@@ -0,0 +1,23 @@
+// Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+var dadosPessoais = {
+  nome: 'Karoliny',
+  sobrenome: 'Pires',
+  dataDeNascimento: '15/04/92'
+}
+// Crie um método no objeto anterior, que mostre o seu nome completo
+var meusDados = {
+  nome: 'Karoliny',
+  
+}
+
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marca: 'Audi',
+}
+
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+
