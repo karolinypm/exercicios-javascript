e570596056ac37c3218b24fb4897b0dbf3cd8531
Karoliny Pires Matias <karoliny.matias@iprev.df.gov.br>
2022-07-04 13:39:45 -0300

    atualização dos objetos e seus métodos

--- objetos/script.js
+++ objetos/script.js
@@ -1,15 +1,17 @@
 // Crie um objeto com os seus dados pessoais
 // Deve possui pelo menos duas propriedades nome e sobrenome
+
+// Crie um método no objeto anterior, que mostre o seu nome completo
 var dadosPessoais = {
   nome: 'Karoliny',
   sobrenome: 'Pires',
-  dataDeNascimento: '15/04/92'
+  dataDeNascimento: '15/04/92',
+  nomeCompleto: function(){
+    return `${this.nome} ${this.sobrenome}`;
+  }
 }
-// Crie um método no objeto anterior, que mostre o seu nome completo
-var meusDados = {
-  nome: 'Karoliny',
-  
-}
+console.log(dadosPessoais)
+console.log(dadosPessoais.nomeCompleto())
 
 // Modifique o valor da propriedade preco para 3000
 var carro = {
@@ -17,7 +19,24 @@ var carro = {
   portas: 4,
   marca: 'Audi',
 }
+console.log(carro);
+carro.preco = 3000;
+console.log(carro.preco);
 
 // Crie um objeto de um cachorro que represente um labrador,
 // preto com 10 anos, que late ao ver um homem
 
+var cachorro = {
+  raca: 'labrador',
+  cor: 'preto',
+  idade: 10,
+  sexo: 'homem',
+  latido: function(sexo){
+    if(sexo === 'homem'){
+      return('au, au, au!')
+    } else{
+      return(silêncio)
+    }
+  }
+}
+console.log(cachorro.latido('homem'))
