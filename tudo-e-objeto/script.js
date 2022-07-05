19d6bd9396c124b0409e04f1b17347c796aa1b48
Karoliny Pires Matias <karoliny.matias@iprev.df.gov.br>
2022-07-05 17:50:31 -0300

    3 Propriedades e métodos de strings

--- tudo-e-objeto/script.js
+++ tudo-e-objeto/script.js
@@ -6,5 +6,6 @@ console.log(frase.charAt(2));
 
 // nomeie 5 propriedades ou métodos de elementos do DOM
 
+
 // busque na web um objeto (método) capaz de interagir com o clipboard, 
 // clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
