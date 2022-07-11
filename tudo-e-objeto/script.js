6d9a839170da2ee60176b2f6168f3c1145c40f0a
Karoliny Pires Matias <karoliny.matias@iprev.df.gov.br>
2022-07-11 14:05:48 -0300

    exercício sobre propriedades e metodos do DOM

--- tudo-e-objeto/script.js
+++ tudo-e-objeto/script.js
@@ -5,7 +5,20 @@ console.log(frase.replace('mundo','Brasil'))
 console.log(frase.charAt(2));
 
 // nomeie 5 propriedades ou métodos de elementos do DOM
+let titulo = document.getElementsByTagName('h1');
+console.log(titulo)
+let texto = document.querySelector('.paragrafo');
+console.log(texto.innerHTML) //apresenta o texto do HTML
+console.log(texto.appendChild)
+console.log(texto.after)
 
 
 // busque na web um objeto (método) capaz de interagir com o clipboard, 
 // clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+let btn = document.querySelector('#copia')
+
+btn.addEventListener('click', function(){
+  let texto = document.querySelector('.copia');
+  texto.select();
+  document.execCommand('copy');
+})
