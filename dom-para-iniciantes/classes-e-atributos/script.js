2d79491481743c7d2cc09a2651f85496ef9c53c2
Karoliny Pires Matias <karoliny.matias@iprev.df.gov.br>
2022-07-14 16:52:57 -0300

    sobre inserção de classes

---
+++ dom-para-iniciantes/classes-e-atributos/script.js
@@ -0,0 +1,10 @@
+// Adicione a classe ativo a todos os parágrafos
+const paragrafo = document.querySelectorAll('p')
+//const paragrafoarray = Array.from(paragrafo)
+paragrafo.classList.add('ativo')
+
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+
+// Verifique se as imagens possuem o atributo alt
+
+// Modifique o href do link externo no menu
