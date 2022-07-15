c23499d0394d9a4a1df3aa2dd5bb06b2003fdb59
karolip.matias <karolinyp.matias@gmail.com>
2022-07-15 16:43:23 -0300

    Atividades sobre manipulação de classes e atributos

--- dom-para-iniciantes/classes-e-atributos/script.js
+++ dom-para-iniciantes/classes-e-atributos/script.js
@@ -1,10 +1,25 @@
-// Adicione a classe ativo a todos os parágrafos
-const paragrafo = document.querySelectorAll('p')
-//const paragrafoarray = Array.from(paragrafo)
-paragrafo.classList.add('ativo')
-
+// Adicione a classe ativo a todos os itens do menu
+const menu = document.querySelectorAll('.menu a')
+menu.forEach((item)=>{
+  item.classList.add('ativo')
+})
 // Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+const menu1 = document.querySelectorAll('.menu a')
+menu.forEach((item)=>{
+  item.classList.remove('ativo')
+})
+menu[0].classList.add('ativo')
 
 // Verifique se as imagens possuem o atributo alt
+const imgs = document.querySelectorAll('img')
+
+imgs.forEach((img)=>{
+  const possuiAtributo = img.hasAttribute('alt');
+  console.log(img,possuiAtributo)
+})
 
 // Modifique o href do link externo no menu
+const linkExterno = document.querySelector('a[href^="http"]');
+linkExterno.setAttribute('href','https://www.google.com')
+console.log(linkExterno)
+
