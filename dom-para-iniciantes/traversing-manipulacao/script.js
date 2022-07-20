098fff575fae6561c41f73103e42f191bdaceb46
Karoliny Pires Matias <karoliny.matias@iprev.df.gov.br>
2022-07-20 14:39:52 -0300

    descrição dos exercícios

--- dom-para-iniciantes/traversing-manipulacao/script.js
+++ dom-para-iniciantes/traversing-manipulacao/script.js
@@ -1,7 +1,20 @@
 // Duplique o menu e adicione ele em copy
+const menu = document.querySelector('.menu')
+const cloneMenu = menu.cloneNode(true)
+const copy = document.querySelector('.copy')
+
+copy.appendChild(cloneMenu);
 
 // Selecione o primeiro DT da dl de Faq
+const primeiroDt = document.querySelector('.faq-lista')
+console.log(primeiroDt)
+console.log(primeiroDt.children[0].innerHTML) 
 
 // Selecione o DD referente ao primeiro DT
+console.log(primeiroDt.children[1])
 
 // Substitua o conteúdo html de .faq pelo de .animais
+const animais = document.querySelector('.animais')
+const faq = document.querySelector('.faq')
+
+faq.replaceChild(animais,faq)
