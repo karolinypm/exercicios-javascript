c0bf94df02310e84f3acf667b6473da371696d3d
karolip.matias <karolinyp.matias@gmail.com>
2022-08-19 10:58:58 -0300

    atividade sobre objetos nativos do JS, host e user

---
+++ objetos/native-host-user/script.js
@@ -0,0 +1,23 @@
+// Liste 5 objetos nativos
+console.log(Object);
+console.log(Array);
+console.log(Function);
+console.log(String);
+console.log(Number);
+
+// Liste 5 objetos do browser
+
+/*querySelectorAll
+HTMLCollection
+NodeList
+Window
+Document
+history*/
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+/*
+** As propriedades web kit foram desenvolvidas para browser chrome
+webkitHidden
+webkitVisibilityState
+*/
