a330c7140abf9b9ed1c07a4a8a39027b6f88e30a
karolip.matias <karolinyp.matias@gmail.com>
2022-08-22 11:04:56 -0300

    exercícios sobre objeto String

---
+++ objetos/string/script.js
@@ -0,0 +1,60 @@
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
+
+const transacoes = [
+  {
+    descricao: 'Taxa do Pão',
+    valor: 'R$ 39',
+  },
+  {
+    descricao: 'Taxa do Mercado',
+    valor: 'R$ 129',
+  },
+  {
+    descricao: 'Recebimento de Cliente',
+    valor: 'R$ 99',
+  },
+  {
+    descricao: 'Taxa do Banco',
+    valor: 'R$ 129',
+  },
+  {
+    descricao: 'Recebimento de Cliente',
+    valor: 'R$ 49',
+  },
+];
+let taxaTotal = 0;
+let recebimentoTotal = 0;
+transacoes.forEach((item)=>{
+  const numeroLimpo = +item.valor.replace('R$ ','')
+  if(item.descricao.slice(0,4)==='Taxa'){
+    taxaTotal += numeroLimpo;
+  }else if(item.descricao.slice(0,4) === 'Rece'){
+    recebimentoTotal += numeroLimpo
+  }
+})
+console.log(taxaTotal);
+console.log(recebimentoTotal);
+
+// Retorne uma array com a lista abaixo
+const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+const arrayTransportes = transportes.split(';')
+console.log(arrayTransportes);
+
+// Substitua todos os span's por a's
+const html = `<ul>
+                <li><span>Sobre</span></li>
+                <li><span>Produtos</span></li>
+                <li><span>Contato</span></li>
+              </ul>`;
+const novoArray = html.split('span')
+const novoHtml = novoArray.join('a');
+console.log(novoHtml);
+
+// Retorne o último caracter da frase
+const frase = 'Melhor do ano!';
+const ultimoCaracter = frase[frase.length-1]
+console.log(ultimoCaracter)
+// Retorne o total de taxas
+
+const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
