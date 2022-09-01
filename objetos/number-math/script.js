750175a105e0493c9458aca868cce1f48024a25b
karolip.matias <karolinyp.matias@gmail.com>
2022-09-01 10:40:40 -0300

    atividade sobre objeto Number e Math

---
+++ objetos/number-math/script.js
@@ -0,0 +1,28 @@
+// Retorne um número aleatório
+// entre 1050 e 2000
+const aleaorio = Math.floor(Math.random() * (2000 - 1050 + 1) + 1050);
+console.log(aleaorio) 
+
+// Retorne o maior número da lista abaixo
+const numeros = '4, 5, 20, 8, 9';
+const arrayNumeros = numeros.split(', ')
+const numeroMax = Math.max(...arrayNumeros);
+console.log(numeroMax);
+
+// Crie uma função para limpar os preços
+// e retornar os números com centavos arredondados
+// depois retorne a soma total
+const listaPrecos = ['R$ 59,99', ' R$ 100,222',
+                     'R$ 230  ', 'r$  200'];
+function limpar(preco){
+  preco = +preco.toUpperCase().replace('R$','').trim().replace(',','.');
+  preco = +preco.toFixed(2);
+  return preco
+}
+let soma = 0
+listaPrecos.forEach((item)=>{
+  soma += limpar(item);
+})
+console.log(soma)
+
+console.log(soma.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}))
